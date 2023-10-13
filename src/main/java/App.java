19b975ee7181d930d089dfe9c298590a7d7244cc
kmra00 <81500636+kmra00@users.noreply.github.com>
2023-10-12 23:54:40 -0300

    alertas y js handling excepciones

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,32 +1,29 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class App {
     public static void main(String[] args) {
-        WebDriver driver =new ChromeDriver();
-        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://www.wikipedia.org");
-        //driver.close();
-        //id
-        Long start = System.currentTimeMillis();
-        driver.findElement(By.id("js-link-box-es"));
-        Long end = System.currentTimeMillis();
-        System.out.println("the time needed to get a id locator es:"+(end - start));
-        //xpath
-        start = System.currentTimeMillis();
-        driver.findElement(By.xpath("//*[@id=\"js-link-box-en\"]"));
-        end = System.currentTimeMillis();
-        System.out.println("the time needed to get a xpath locator es:"+(end - start));
+        driver.manage().window().maximize();
+        driver.get("https://es.secure.imvu.com/welcome/ftux/");
 
-        //css
-        start = System.currentTimeMillis();
-        driver.findElement(By.cssSelector("#www-wikipedia-org > div.central-featured > div.central-featured-lang.lang2"));
-        end = System.currentTimeMillis();
-        System.out.println("the time needed to get a css locator es:"+(end - start));
+        WebElement registroBoton = driver.findElement(By.cssSelector("#imvu > nav > ul.secondary-nav > button"));
+        //
+        ((JavascriptExecutor)driver).executeScript("arguments[0].click();",registroBoton);
+        //settings timeouts
+        ((JavascriptExecutor) driver).executeAsyncScript("window.setTimeout(arguments[arguments.length-1], 1000);");
+        //cambiando la web page
+
+        //((JavascriptExecutor)driver).executeScript("window.location='https://wikipedia.com'");
+
+        //scroll
+        ((JavascriptExecutor)driver).executeScript("window.scrollBy(0,1000)");
+        //driver.close();
+
 
     }
 }
--- src/main/java/SeleDemo.java
+++ src/main/java/SeleDemo.java
@@ -1,29 +1,48 @@
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.time.Duration;
 
 public class SeleDemo {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-
+        //Implicit Wait
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get("Y://Curso_Selenium//Selenium1//tablePage.html");
+        driver.get("https://www.wikipedia.org/");
 
-       // System.out.println(driver.findElement(By.xpath("/html/body/table/tbody[1]/tr[2]/td[1]")).getText());
-        //System.out.println(driver.findElement(By.xpath("/html/body/table/tbody[1]/tr[1]/th[2]")).getText());
+        System.out.println(driver.getTitle());
 
-        List<WebElement> listaWeb = driver.findElements(By.xpath("/html/body/table/tbody[1]/tr"));
-        for (WebElement element : listaWeb){
-            System.out.println(element.getText());
-        }
+        //Explicit Wait
 
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("js-link-box-en"))));
+
+        //Fluent Wait
+
+        Wait fluentWait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(15))
+                .pollingEvery(Duration.ofSeconds(5))
+                .ignoring(NoSuchElementException.class);
+
+        fluentWait.until(new Function<WebDriver, WebElement>(){
+
+            public WebElement apply(WebDriver driver ) {
+                return driver.findElement(By.id("js-link-box-en"));
+            }
+        });
 
         driver.close();
     }
